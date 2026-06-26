fb395d1e1c03363ac6d4938cc302d544027f38a1
오승훈 <abffks3@naver.com>
2026-06-26 22:28:19 +0900

    버튼 추가1

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ document.querySelectorAll('a[href^="#"]').forEach((link) => {
     target.scrollIntoView({ behavior: "smooth", block: "start" });
   });
 });
+
+const scrollTopButton = document.querySelector(".scroll-top");
+
+function updateScrollTopButton() {
+  scrollTopButton.classList.toggle("is-visible", window.scrollY > 500);
+}
+
+scrollTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+window.addEventListener("scroll", updateScrollTopButton);
+updateScrollTopButton();
